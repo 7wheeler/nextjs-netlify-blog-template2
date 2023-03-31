cb56781b0c62e9f9df658f4d119504931b6b4e23
7wheeler <7wheeler@gmail.com>
2023-03-31 15:54:32 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            Hi, We're Happy to see you!<span className="fancy">.</span>
           </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <span className="handle">7wheeler@gmail.com</span>
+          <h2>Yours And Mine Designs</h2>
           <SocialList />
         </div>
       </div>
